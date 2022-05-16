8b40e38875cae060017bd114524b8dba4d5c9fa4
andrea.giulianelli <andrygiulio@gmail.com>
2022-05-16 15:50:05 +0200

    2.2

--- lab.pl
+++ lab.pl
@@ -8,7 +8,7 @@ dropAny(X, [H | Xs], [H | L]) :- dropAny(X, Xs, L).
 dropFirst(X, L, T) :- dropAny(X, L, T), !.
 
 % dropLast: drops only the last occurrence (showing no alternative results)
-dropLast(X, [H | Xs], [H | L]) :- write(H), nl, dropLast(X, Xs, L), !.
+dropLast(X, [H | Xs], [H | L]) :- dropLast(X, Xs, L), !.
 dropLast(X, [X | T], T).
 
 % dropAll: drop all occurrences, returning a single list as result
@@ -17,6 +17,26 @@ dropAll(X, [X | T], R) :- dropAll(X, T, R), !.
 dropAll(X, [H | Xs], [H | L]) :- dropAll(X, Xs, L), !.
 
 % ======= 2 =======
+% 2.1
 % fromList (+ List , - Graph )
-fromList ([_], []).
-fromList ([H1, H2 | T], [e(H1, H2) | L]) :- fromList([H2 | T], L).
+fromList([_], []).
+fromList([H1, H2 | T], [e(H1, H2) | L]) :- fromList([H2 | T], L).
+
+% 2.2
+% fromCircList(+ List, -Graph)
+fromCircList([H | T], G) :- fromCircList([H | T], G, H).
+fromCircList([X], [e(X, H)], H). 
+fromCircList([H1, H2 | T], [e(H1, H2) | L], H) :- fromCircList([H2 | T], L, H).
+
+% 2.3
+% inDegree(+Graph, +Node, -Deg)
+% Deg is the number of edges leading into Node
+% TODO: non-tail
+
+% tail
+inDegree(L, X, N) :-  inDegree(L, X, N, 0).
+inDegree([], X, N, N).
+inDegree([e(_, X) | T], X, N, Nold) :- N2 is Nold + 1, inDegree(T, X, N, N2), !. % tail
+inDegree([e(_, Y) | T], X, N, Nold) :- inDegree(T, X, N, Nold).
+
+
